d865f4755cb2893bf6429972d94ba178f9eb6f16
acabrun <acabrun@gmail.com>
2020-03-19 11:10:49 +0100

    Setup display

--- App.js
+++ App.js
@@ -4,13 +4,25 @@ import { Text, View, Button } from "react-native";
 export default class App extends Component {
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: 'yellow' }}>
+      <View style={{ flex: 1, backgroundColor: "yellow" }}>
         <View
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          style={{ flex: 0.5, justifyContent: "center", backgroundColor: "white" }}
+        ></View>
+        <View
+          style={{ flex: 0.8, justifyContent: "center", alignItems: "center" }}
         >
           <Text>7 Wonder Score</Text>
         </View>
-        <View>
+        <View
+          style={{ flex: 7, justifyContent: "center", backgroundColor: "blue" }}
+        ></View>
+        <View
+          style={{
+            flex: 1.1,
+            justifyContent: "space-around",
+            backgroundColor: "orange",
+          }}
+        >
           <Button title="Continue" color="green" onPress={() => null} />
           <Button title="New game" onPress={() => null} />
         </View>
